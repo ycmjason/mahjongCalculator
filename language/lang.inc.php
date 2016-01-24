4845c7192ac8ec10f5d8716825f70f12eb08f557
Jason Yu <jason.yu14@imperial.ac.uk>
2016-01-24 11:24:38 +0000

    set default language to traditional chinese

--- language/lang.inc.php
+++ language/lang.inc.php
@@ -3,6 +3,6 @@ function valid_language($language){
   $valid_languages = array("chinglish", "traditional_chinese");
   return in_array($language, $valid_languages);
 }
-$lang = valid_language($_GET['lang']) ? $_GET['lang'] : "chinglish";
+$lang = valid_language($_GET['lang']) ? $_GET['lang'] : "traditional_chinese";
 require_once("./language/".$lang.".inc.php");
 ?>
