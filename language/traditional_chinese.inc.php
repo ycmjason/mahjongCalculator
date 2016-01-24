782dc7b8dd1deda25f23372e58fdc264d4c5af55
Jason Yu <ycm.jason@gmail.com>
2016-01-24 10:25:04 +0000

    added chinese language pack

---
+++ language/traditional_chinese.inc.php
@@ -0,0 +1,46 @@
+<?php
+/* chinglish lang */
+$lang = array(
+  "farn" => "番",
+  "self_touch" => "自摸",
+  // index.php
+  "title"                        => "麻雀計分器",
+
+  // entrance.html
+  "join_existing_game"           => "加入牌局",
+  "failed_joining_game"          => "找不到此牌局",
+  "number_of_players"            => "玩家人數",
+  "player_n_name"                => "玩家 {{i}} 的名字",
+  "advanced_setting"             => "進階設定",
+  "start_button_msg"             => "開始牌局",
+  "upload_button_msg"            => "上載牌局",
+
+  // started.html
+  "this_game_code"               => "此局密碼",
+  "download_button_msg"          => "下載牌局",
+  "cancel_last_round_button_msg" => "取消上回",
+  "undo_eat_button_msg"          => "取消食糊",
+  "flush_game_button_msg"        => "流局",
+  "losers"                       => "輸家",
+  "out_chung_button_msg"         => "出銃",
+  "other_losers"                 => "其他輸家",
+  "add_game_button_msg"          => "新增玩家",
+
+  // advancedSetting.html
+  "chungness"                    => "銃制",
+  "chungness_half_chung"         => "半銃",
+  "chungness_full_chung"         => "全銃",
+  "hk_tradition"                 => "香港傳統",
+  "scoring_method"               => "籌碼計法",
+  "scoring_method_25Chicken"     => "二五雞",
+  "scoring_method_51"            => "五一",
+  "scoring_method_12Mosquitos"   => "一二蚊",
+  "half_spicy_from"              => "幾番開始半辣？",
+  "half_spicy_from_never"        => "從不半辣",
+  "half_spicy_from_4"            => "4 番",
+
+  //summaryTable.html
+  "round"   => "回合",
+  "summary" => "總共"
+);
+?>
